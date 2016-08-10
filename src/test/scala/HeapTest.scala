e78233ac6a787b7c66b44cd6139392418b214eb9
Chris Okasaki <cokasaki@gmail.com>
2016-08-10 13:21:09 -0400

    refactored HeapTest and added more tests
    Reduced code duplication between tests for min-heaps and max-heaps.

--- src/test/scala/HeapTest.scala
+++ src/test/scala/HeapTest.scala
@@ -1,6 +1,10 @@
 // Resuable tests for different heap implementations.
 // See LeftistHeapTest.scala and SkewHeapTest.scala for examples of usage.
-// TODO: Needs more tests!
+// TODO: Needs more tests, especially for pairedFactory
+// TODO: Fix tests to account for the fact that "equal" elements according to
+//   the ordering are not necessarily equal.  Therefore, it's not enough to test
+//   (for example) that the first element in the heap is == the first element
+//   in the sorted list.
 
 import scads.immutable._
 import org.scalacheck._
@@ -11,131 +15,145 @@ trait HeapTest { self: Properties =>
   def companion: HeapCompanion
 
   type E = Int // the element type used for most/all of the tests
-  val minFactory: HeapFactory { type Elem = E } = companion.Min.factory[E]
-  val maxFactory: HeapFactory { type Elem = E } = companion.Max.factory[E]
 
-  property("minFactory.from") = forAll { list: List[E] =>
-    val h = minFactory.from(list)
-    h.checkInvariant()
-    h.sorted.toList == list.sorted
-  }
-  property("maxFactory.from") = forAll { list: List[E] =>
-    val h = maxFactory.from(list)
-    h.checkInvariant()
-    h.sorted.toList == list.sorted.reverse
-  }
-  property("isEmpty/size (minFactory)") = forAll {list: List[E] =>
-    val h = minFactory.from(list)
-    h.checkInvariant()
-    h.isEmpty == list.isEmpty && h.size == list.size
-  }
-  property("isEmpty/size (maxFactory)") = forAll {list: List[E] =>
-    val h = maxFactory.from(list)
-    h.checkInvariant()
-    h.isEmpty == list.isEmpty && h.size == list.size
-  }
-  property("add (minFactory)") = forAll { (list: List[E], x: E) =>
-    val h = minFactory.from(list)
-    val h2 = h.add(x)
-    h2.checkInvariant()
-    h2.sorted.toList == (x +: list).sorted
-  }
-  property("add (maxFactory)") = forAll { (list: List[E], x: E) =>
-    val h = maxFactory.from(list)
-    val h2 = h.add(x)
-    h2.checkInvariant()
-    h2.sorted.toList == (x +: list).sorted.reverse
-  }
-  property("first (minFactory)") = forAll { list: List[E] =>
-    val h = minFactory.from(list)
-    val fst = scala.util.Try(h.first)
-    if (list.isEmpty) fst.isFailure
-    else fst.isSuccess && fst.get == list.min
-  }
-  property("first (maxFactory)") = forAll { list: List[E] =>
-    val h = maxFactory.from(list)
-    val fst = scala.util.Try(h.first)
-    if (list.isEmpty) fst.isFailure
-    else fst.isSuccess && fst.get == list.max
-  }
-  property("rest (minFactory)") = forAll { list: List[E] =>
-    val h = minFactory.from(list)
-    val r = scala.util.Try(h.rest)
-    if (list.isEmpty) r.isFailure
-    else {
-      r.get.checkInvariant()
-      r.isSuccess && r.get.sorted.toList == list.sorted.tail
+  def test(isMin: Boolean) {
+    val name = if (isMin) "Min." else "Max."
+    val hname = name + "factory.Heap."
+    val hff: HeapFactoryFactory = if (isMin) companion.Min else companion.Max
+    val factory = hff.factory[E]
+    def sort(list: List[E]): List[E] = if (isMin) list.sorted else list.sorted.reverse
+    def sameElements(heap: SHeap[E], list: List[E]): Boolean = heap.sorted.toList == sort(list)
+    def firstOption(list: List[E]): Option[E] = sort(list).headOption
+    val ord: Ordering[Int] = if (isMin) Ordering.Int else Ordering.Int.reverse
+
+    property(name + "factory.from") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      h.checkInvariant()
+      sameElements(h, list)
+    }
+    property(name + "factory.apply") = forAll { list: List[E] =>
+      val h = factory.apply(list : _*)
+      h.checkInvariant()
+      sameElements(h, list)
+    }
+    property(name + "factory.newBuilder") = forAll { list: List[E] =>
+      val builder = factory.newBuilder
+      for (x <- list) builder += x
+      val h = builder.result
+      h.checkInvariant()
+      sameElements(h, list)
+    }
+    property(name + "factory._fromSorted") = forAll { list: List[E] =>
+      val s = sort(list)
+      val h = factory._fromSorted(s)
+      h.checkInvariant()
+      sameElements(h, s)
+    }
+    property(name + "factory._fromReverseSorted") = forAll { list: List[E] =>
+      val s = sort(list).reverse
+      val h = factory._fromReverseSorted(s)
+      h.checkInvariant()
+      sameElements(h, s)
+    }
+    property(name + "from") = forAll { list: List[E] =>
+      val h = hff.from(list)
+      h.checkInvariant()
+      sameElements(h, list)
+    }
+    property(name + "apply") = forAll { list: List[E] =>
+      val h = hff.apply(list : _*)
+      h.checkInvariant()
+      sameElements(h, list)
+    }
+    property(hname + "isEmpty/nonEmpty") = forAll {list: List[E] =>
+      val h = factory.from(list)
+      h.isEmpty == list.isEmpty && h.nonEmpty == list.nonEmpty
+    }
+    property(hname + "size") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      h.size == list.size
+    }
+    property(hname + "add") = forAll { (list: List[E], x: E) =>
+      val h = factory.from(list)
+      val h2 = h.add(x)
+      h2.checkInvariant()
+      sameElements(h2, x +: list)
+    }
+    property(hname + "_addFirst") = forAll { list: List[E] => list.nonEmpty ==> {
+      val x = firstOption(list).get
+      val listWithoutX = list.takeWhile(_ != x) ++ list.dropWhile(_ != x).tail
+      val h = factory.from(listWithoutX)
+      val h2 = h._addFirst(x)
+      h2.checkInvariant()
+      sameElements(h2, list)
+    }}
+    property(hname + "first") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      scala.util.Try(h.first).toOption == firstOption(list)
+    }
+    property(hname + "firstOption") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      h.firstOption == firstOption(list)
+    }
+    property(hname + "rest") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      val r = scala.util.Try(h.rest)
+      if (list.isEmpty) r.isFailure
+      else {
+        val h2 = r.get
+        h2.checkInvariant()
+        sameElements(h2, sort(list).tail)
+      }
+    }
+    property(hname + "firstView") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      val s = sort(list)
+      h.firstView match {
+        case None => list.isEmpty
+        case Some((x,h2)) =>
+        h2.checkInvariant()
+        x == s.head && sameElements(h2, s.tail)
+      }
+    }
+    property(hname + "merge") = forAll { (list1: List[E], list2: List[E]) =>
+      val h1 = factory.from(list1)
+      val h2 = factory.from(list2)
+      val merged = h1.merge(h2)
+      merged.checkInvariant()
+      sameElements(merged, list1 ++ list2)
+    }
+    property(hname + "takeUntil") = forAll { (list: List[E], x: E) =>
+      val h = factory.from(list)
+      val h2 = h.takeUntil(x)
+      h2.checkInvariant()
+      sameElements(h2, list.filter(ord.lt(_,x)))
+    }
+    property(hname + "takeTo") = forAll { (list: List[E], x: E) =>
+      val h = factory.from(list)
+      val h2 = h.takeTo(x)
+      h2.checkInvariant()
+      sameElements(h2, list.filter(ord.lteq(_,x)))
+    }
+    property(hname + "dropUntil") = forAll { (list: List[E], x: E) =>
+      val h = factory.from(list)
+      val h2 = h.dropUntil(x)
+      h2.checkInvariant()
+      sameElements(h2, list.filter(ord.gteq(_,x)))
+    }
+    property(hname + "dropTo") = forAll { (list: List[E], x: E) =>
+      val h = factory.from(list)
+      val h2 = h.dropTo(x)
+      h2.checkInvariant()
+      sameElements(h2, list.filter(ord.gt(_,x)))
+    }
+    property(hname + "iterator") = forAll { list: List[E] =>
+      val h = factory.from(list)
+      val list2 = h.iterator.toList
+      list2.sorted == list.sorted
     }
   }
-  property("rest (maxFactory)") = forAll { list: List[E] =>
-    val h = maxFactory.from(list)
-    val r = scala.util.Try(h.rest)
-    if (list.isEmpty) r.isFailure
-    else {
-      r.get.checkInvariant()
-      r.isSuccess && r.get.sorted.toList == list.sorted.reverse.tail
-    }
-  }
-  property("merge (minFactory)") = forAll { (list1: List[E], list2: List[E]) =>
-    val h1 = minFactory.from(list1)
-    val h2 = minFactory.from(list2)
-    val merged = h1.merge(h2)
-    merged.checkInvariant()
-    merged.sorted.toList == (list1 ++ list2).sorted
-  }
-  property("merge (maxFactory)") = forAll { (list1: List[E], list2: List[E]) =>
-    val h1 = maxFactory.from(list1)
-    val h2 = maxFactory.from(list2)
-    val merged = h1.merge(h2)
-    merged.checkInvariant()
-    merged.sorted.toList == (list1 ++ list2).sorted.reverse
-  }
-  property("takeUntil (minFactory)") = forAll { (list: List[E], x: E) =>
-    val h = minFactory.from(list)
-    val hout = h.takeUntil(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ < x).sorted
-  }
-  property("takeTo (minFactory)") = forAll { (list: List[E], x: E) =>
-    val h = minFactory.from(list)
-    val hout = h.takeTo(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ <= x).sorted
-  }
-  property("dropUntil (minFactory)") = forAll { (list: List[E], x: E) =>
-    val h = minFactory.from(list)
-    val hout = h.dropUntil(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ >= x).sorted
-  }
-  property("dropTo (minFactory)") = forAll { (list: List[E], x: E) =>
-    val h = minFactory.from(list)
-    val hout = h.dropTo(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ > x).sorted
-  }
-  property("takeUntil (maxFactory)") = forAll { (list: List[E], x: E) =>
-    val h = maxFactory.from(list)
-    val hout = h.takeUntil(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ > x).sorted.reverse
-  }
-  property("takeTo (maxFactory)") = forAll { (list: List[E], x: E) =>
-    val h = maxFactory.from(list)
-    val hout = h.takeTo(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ >= x).sorted.reverse
-  }
-  property("dropUntil (maxFactory)") = forAll { (list: List[E], x: E) =>
-    val h = maxFactory.from(list)
-    val hout = h.dropUntil(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ <= x).sorted.reverse
-  }
-  property("dropTo (maxFactory)") = forAll { (list: List[E], x: E) =>
-    val h = maxFactory.from(list)
-    val hout = h.dropTo(x)
-    hout.checkInvariant()
-    hout.sorted.toList == list.filter(_ < x).sorted.reverse
-  }
+
+  test(true)
+  test(false)
+
 }
